401cdb83dac9c3c7966b5986bba1e5cb008b5108
harry0000 <zan00753@nifty.ne.jp>
2024-09-01 20:28:58 +0900

    Use sourcecode instead of macro

--- infra/src/main/scala-2/io/github/harry0000/Extension.scala
+++ infra/src/main/scala-2/io/github/harry0000/Extension.scala
@@ -1,13 +1,21 @@
 package io.github.harry0000
 
-import scala.language.experimental.macros
 import scala.language.implicitConversions
 
-object Extension {
-  class TableExtensionEx[E](table: Table[E]) extends TableExtension[E](table) {
-    override def result: String = macro CallerResolver.resolve[String]
+trait ExtensionBase {
+  implicit def tableToTableExtension[E](table: Table[E]): TableExtension[E] =
+    new TableExtension[E](table)
+}
+
+trait Extension { this: ExtensionBase =>
+  class TableExtensionEx[E](
+    table: Table[E]
+  )(implicit enclosing: sourcecode.Enclosing) extends TableExtension[E](table) {
+    override def result: String = enclosing.value
   }
 
-  implicit def tableToTableExtension[E](table: Table[E]): TableExtensionEx[E] =
+  implicit def tableToTableExtensionEx[E](
+    table: Table[E]
+  )(implicit enclosing: sourcecode.Enclosing): TableExtensionEx[E] =
     new TableExtensionEx[E](table)
 }
